1f6e01a79dc269e3a165948c9bd9e2fdfdf09e1e
Rod Johnson <johnsonroda@gmail.com>
2018-02-14 11:05:59 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/elbe/ElbeApplication.java
@@ -0,0 +1,12 @@
+package com.atomist.elbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElbeApplication.class, args);
+	}
+}
